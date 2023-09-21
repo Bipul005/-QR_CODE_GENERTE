0f96884b47a332054c89f195febce97159ee4e6c
Bipul kumar <94814946+Bipul005@users.noreply.github.com>
2023-09-21 19:21:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+    message: "type url",
+    name: "url",
+    },
+  ])
+  .then((answers) => {
+    
+    const url = answers.url;
+    var qr_svg=qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+   
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
